158392b549f30b3c0afdb5d664ad6e9275fc12fb
Tom Lam <lamktom@yahoo.com>
2018-11-28 12:08:56 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// for help: http://jsfiddle.net/z66WF/
+
+// let num = 0; 
+
+// function less {
+// 	num -= 1; 
+// 	document.getElementById('num').innerHTML = nums;
+// }
+
+// function more {
+// 	num += 1; 
+// 	document.getElementById('num').innerHTML = nums;
+// }
